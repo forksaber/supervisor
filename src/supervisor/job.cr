94f4bad308a99c40ef2d0df72a5d65a3a0c0b767
Neeraj Bhunwal <neeraj.bhunwal@gmail.com>
2017-01-18 04:14:38 +0530

    job.cr: use macros to reduce redundant code

--- src/supervisor/job.cr
+++ src/supervisor/job.cr
@@ -1,41 +1,29 @@
 module Supervisor
   class Job
-    STRING_ATTRS = ["name", "working_dir", "stopsignal", "command", "stdout_logfile", "stderr_logfile"]
-    BOOL_ATTRS   = ["autorestart", "stopasgroup", "killasgroup", "redirect_stderr"]
-    HASH_ATTRS   = ["env"]
-    INT_ATTRS    = ["numprocs", "startsecs", "startretries", "stopwaitsecs"]
 
-    getter :name, :working_dir, :stopsignal
-    {% for i in BOOL_ATTRS %}
-    getter {{i}}
-    {% end %}
+    getter name = ""
+    getter working_dir = Dir.current
+    getter stopsignal = "TERM"
 
-    {% for i in INT_ATTRS %}
-    getter {{i}}
-    {% end %}
+    @command = ""
+    @stdout_logfile = "/dev/null"
+    @stderr_logfile = ""
 
-    getter :env
-    @name : String
+    getter env = {} of String => String
+
+    getter autorestart = true
+    getter stopasgroup = false
+    getter killasgroup = true
+    getter redirect_stderr = true
+
+    getter startsecs = 1
+    getter startretries = 3
+    getter stopwaitsecs = 10
+
+    def_hash @name, @working_dir, @stdout_logfile,
+             @stderr_logfile, @redirect_stderr
 
     def initialize(attrs)
-      @name = ""
-      @command = ""
-      @working_dir = Dir.current
-      @stdout_logfile = "/dev/null"
-      @stderr_logfile = ""
-      @stopsignal = "TERM"
-
-      @env = {} of String => String
-
-      @autorestart = true
-      @stopasgroup = false
-      @killasgroup = true
-      @redirect_stderr = true
-
-      @numprocs = 0_u32
-      @startsecs = 1_u32
-      @startretries = 3_u32
-      @stopwaitsecs = 10_u32
       update(attrs)
     end
 
@@ -52,55 +40,28 @@ module Supervisor
       absolute_path @stderr_logfile
     end
 
-    private def update(attrs)
-      {% for ivar, i in @type.instance_vars %}
-        puts "@{{ivar.type}}"
-      {% end %}
-      puts {{@type}}
+    def update(attrs)
       attrs.each do |key, value|
         set key, value
       end
     end
 
     private def set(key, value)
+      {% begin %}
       case key
-      when "name"
-        @name = value.as(String)
-      when "command"
-        @command = value.as(String)
-      when "working_dir"
-        @working_dir = value.as(String)
-      when "stdout_logfile"
-        @stdout_logfile = value.as(String)
-      when "stderr_logfile"
-        @stderr_logfile = value.as(String)
-      when "stopsignal"
-        @stopsignal = value.as(String)
-      when "autorestart"
-        @autorestart = value.as(Bool)
-      when "stopasgroup"
-        @stopasgroup = value.as(Bool)
-      when "killasgroup"
-        @killasgroup = value.as(Bool)
-      when "redirect_stderr"
-        @redirect_stderr = value.as(Bool)
-      when "numprocs"
-        @numprocs = value.as(UInt32)
-      when "startsecs"
-        @startsecs = value.as(UInt32)
-      when "startretries"
-        @startretries = value.as(UInt32)
-      when "stopwaitsecs"
-        @stopwaitsecs = value.as(UInt32)
-      when "env"
-        @env = value.as(Hash(String, String))
+      {% for ivar in @type.instance_vars %}
+      when "{{ivar.id}}"
+        @{{ivar.id}} = value.as({{ivar.type}})
+      {% end %}
       else
         raise "no such key #{key}"
       end
+      {% end %}
     end
 
     private def absolute_path(path)
       File.expand_path(path, @working_dir)
     end
+
   end
 end
