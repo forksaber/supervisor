ae71439b8750b20077bcc6beeabc5cb2b71a9461
Neeraj Bhunwal <neeraj.bhunwal@gmail.com>
2017-02-23 18:12:19 +0530

    supervisor.cr: updates

--- src/supervisor.cr
+++ src/supervisor.cr
@@ -5,26 +5,21 @@ module Supervisor
   # TODO Put your code here
 end
 
-
-
 jobs_config = Array(Hash(String, (String | Hash(String, String) | Int32 | Bool))).from_json(File.read("config/jobs.yml"))
 puts jobs_config
 
 jobs = [] of Supervisor::Job
 jobs_config.each do |job|
   j = Supervisor::Job.new(job)
-#  puts j.inspect
-#  puts j.command
-#  puts j.stdout_logfile
   jobs << j
 end
 
 channel = Channel(Nil).new
 
-#jobs = [] of Supervisor::Job
 process = Supervisor::Process.new(jobs[0])
-process.run
+out = process.start
+puts "post start"
+puts process.stop
+puts "post stop"
 
-2.times do
-  channel.receive
-end
+channel.receive
