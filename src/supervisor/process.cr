6f08b3e3bf2280f4f73c3a544a6eb37dbeb84fac
Neeraj Bhunwal <neeraj.bhunwal@gmail.com>
2017-02-23 17:55:39 +0530

    move statemachine to a separate class, add sync start/stop apis to process

--- src/supervisor/process.cr
+++ src/supervisor/process.cr
@@ -1,146 +1,153 @@
+require "./state_machine"
+require "./custom_process"
+
 module Supervisor
   class Process
 
-    enum State
-      STOPPED
-      FATAL
-      EXITED
-      STARTING
-      BACKOFF
-      RUNNING
-      STOPPING
-    end
-
-    enum Event
-      START
-      STOP
-      STARTED
-      EXITED
-      RETRY
-      FATAL
-    end
- 
-    getter state = State::STOPPED
     getter job : Job
-
-    @try_count = 0
-    @chan = Channel(Event).new
     @process : (::Process | Nil)
+    @name : String
+    @fsm : StateMachine
+    @stdout : File?
+    @stderr : File?
 
-    def initialize(@job)
-      listen
+    delegate state, to: @fsm
+
+    def initialize(@job, name = nil)
+      @name = name || @job.name
+      @fsm = StateMachine.new(
+        retries: @job.startretries,
+        start_proc: -> (callback : EventCallback?) { start_process(callback); nil },
+        stop_proc: -> { process = @process; stop_process(process, @job.stopwaitsecs) if process; nil },
+        autorestart: @job.autorestart
+      )
     end
 
-    def stop_async
-      send_event Event::STOP
+    def run 
+      fire Event::START, nil
     end
 
-    def run
-      send_event Event::START
-    end
+    def start
+      chan = Channel({Bool, State, Bool}).new
+      callback = ->(prev : State, current : State) do
+        puts({prev, current})
+        changed = (prev != current)
+        already_started = current.started? && !changed
 
-    private def listen
-      spawn do
-        loop do
-          event = @chan.receive
-          state_machine event
-          puts "new state: #{@state}"
+        puts ">> #{@name}: #{prev} -> #{current}"
+        if already_started
+          puts "already running"
+          chan.send({true, current, changed})
+        elsif current == State::RUNNING
+          chan.send({true, current, changed})
+        elsif current.stopped?
+          chan.send({false, current, changed})
         end
+        nil
       end
+      fire Event::START, callback
+      chan.receive
     end
 
-    private def state_machine(event : Event)
-      case {@state, event}
+    def stop
+      chan = Channel({Bool, State, Bool}).new(1)
+      callback = ->(prev : State, current : State) do
+        changed = (prev != current)
 
-      when {State::STOPPED, Event::START}
-        @state = State::STARTING
-        start
-      when {State::FATAL, Event::START}
-        @state = State::STARTING
-        start
-      when {State::EXITED, Event::START}
-        @state = State::STARTING
-        start
-
-      when {State::STARTING, Event::STARTED}
-        @state = State::RUNNING
-      when {State::STARTING, Event::EXITED}
-        @state = State::BACKOFF
-        @try_count += 1
-        if @try_count >= @job.startretries
-          send_event Event::FATAL
-        else
-          send_event Event::RETRY
+        puts "#{@name} -> #{current}"
+        if current.stopped?
+          chan.send({true, current, changed})
         end
-
-      when {State::BACKOFF, Event::RETRY}
-        @state = State::STARTING
-        start
-      when {State::BACKOFF, Event::FATAL}
-        @state = State::FATAL
-
-      when {State::RUNNING, Event::STOP}
-        @state = State::STOPPING
-        stop
-      when {State::RUNNING, Event::EXITED}
-        @state = State::EXITED
-        send_event Event::START if @job.autorestart
-
-      when {State::STOPPING, Event::EXITED}
-        @state = State::STOPPED
-        @try_count = 0
-      else
-        puts "unknown state/event combo: #{@state}, #{event}"
+        nil
       end
+      fire Event::STOP, callback
+      x = chan.receive
     end
 
-    private def send_event(event : Event)
-      spawn { @chan.send event }
+    private def fire(*args, **kwargs)
+      @fsm.fire(*args, **kwargs)
     end
 
-    private def start
+    private def start_process(callback : EventCallback?)
+      spawn_chan = Channel(Nil).new(1)
+      exception_chan = Channel(Nil).new(1)
       spawn do
-        exited = false
-        wait_chan = Channel(Nil).new
+        exit_chan = Channel(Nil).new(1)
+        start_chan = Channel(Nil).new(1)
         begin
           stdout = File.open(@job.stdout_logfile, "a+")
-          stderr = @job.stderr_logfile ? File.open(@job.stderr_logfile.as(String), "a+") : stdout
+          stderr = @job.stderr_logfile ? File.open(@job.stderr_logfile.as(String), "a+") : File.open(@job.stdout_logfile, "a+")
           stdout.flush_on_newline = true
           stderr.flush_on_newline = true
-
+          @stdout = stdout
+          @stderr = stderr
+          
           ::Process.run(**spawn_opts) do |process|
-            spawn { sleep @job.startsecs; send_event Event::STARTED unless exited}
             @process = process
-            puts process.inspect
-            spawn do 
+            spawn_chan.send nil
+            spawn do
+              sleep @job.startsecs
+              select
+              when exit_chan.receive
+              else
+                start_chan.send nil
+                fire Event::STARTED, callback
+              end
+            end
+            
+            wait_chan = Channel(Nil).new(1)
+            spawn do
               process.error.each_line { |l| stderr.not_nil!.puts l }
               wait_chan.send nil
             end
             process.output.each_line { |l| stdout.puts l }
+            wait_chan.receive
           end
 
-          wait_chan.receive
         rescue e
-          puts e
+          puts "exception : #{e}"
+          exception_chan.send nil
+          process = @process
+          stop_process(process, 1) if process
         ensure
-          exited = true
-          send_event Event::EXITED
+          @process.try &.wait
+          @process = nil
           stdout.close if stdout
           stderr.try &.close if stderr
+          @stdout = nil
+          @stderr = nil
+          exit_chan.send nil
+          select
+          when start_chan.receive
+            fire Event::EXITED, nil
+          else
+            puts "Exited"
+            fire Event::EXITED, callback, async: true
+          end
         end
+      end
+
+      select
+      when spawn_chan.receive
+        puts "received"
+      when exception_chan.receive
+        puts "ex-received"
       end
     end
 
-    private def stop
-      spawn do
-        process = @process
-        process.kill if process
-        sleep @job.stopwaitsecs
-        if (process && !process.terminated?)
-          process.kill(Signal::KILL)
-          puts "Killed #{process.pid}"
-        end
+    private def stop_process(process : ::Process, stop_wait)
+      process.kill
+      sleep 0.05
+      return if process.terminated?
+      sleep stop_wait
+      if process.exists?
+        group_pid = 0 - process.pid
+        puts group_pid
+        ::Process.kill(Signal::KILL, group_pid)
+        puts "Killed #{process.pid}"
       end
+    rescue e
+      puts "#{e}, #{process.pid}"
     end
 
     private def spawn_opts
