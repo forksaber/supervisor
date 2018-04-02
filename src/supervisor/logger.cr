73607dab7b9199dcdca27044f125461ecc01ab7c
Neeraj <neeraj.bhunwal@gmail.com>
2018-04-02 07:23:56 +0530

    move callbacks to state_stream, add logger

---
+++ src/supervisor/logger.cr
@@ -0,0 +1,25 @@
+module Supervisor
+  module Logger
+    @@logger = ::Logger.new(STDOUT)
+    @@stderr = ::Logger.new(STDERR)
+    def self.logger
+      @@logger
+    end
+
+    def self.stderr
+      @@stderr
+    end
+
+    def logger
+      ::Supervisor::Logger.logger
+    end
+
+    def stderr
+      ::Supervisor::Logger.stderr
+    end
+
+    def debug?
+      logger.level == ::Logger::DEBUG
+    end
+  end
+end
--- src/supervisor/state_machine.cr
+++ src/supervisor/state_machine.cr
@@ -1,5 +1,5 @@
-module Supervisor   
-  
+module Supervisor
+
   enum State
     STOPPED
     FATAL
@@ -16,6 +16,10 @@ module Supervisor
     def started?
       [STARTING, BACKOFF, RUNNING].includes? self
     end
+
+    def running?
+      self == RUNNING
+    end
   end
 
   enum Event
@@ -26,58 +30,59 @@ module Supervisor
     RETRY
     FATAL
   end
- 
+
   alias EventCallback = Proc(State, State, Nil)
 
   class StateMachine
 
+    include Logger
+
     getter state = State::STOPPED
-    getter start_proc : Proc(EventCallback?, Void)
+    getter start_proc : Proc(Void)
     getter stop_proc : Proc(Void)
 
     @try_count = 0
-    @chan = Channel({Event, EventCallback?}).new
+    @chan = Channel(Event).new
     @retries : Int32
     @autorestart : Bool
-    @stop_callback : EventCallback?
+    @state_stream : StateStream
 
-    def initialize(@retries, @start_proc, @stop_proc, @autorestart)
+    def initialize(@retries, @start_proc, @stop_proc, @autorestart, @state_stream)
       listen
     end
 
-    def fire(event : Supervisor::Event, callback : EventCallback? = nil, async = false)
+    def fire(event : Supervisor::Event, async = false)
       if async
-        @chan.send({event, callback}) 
+        @chan.send(event)
       else
-        spawn { @chan.send({event, callback}) }
+        spawn { @chan.send(event) }
       end
     end
 
     private def listen
       spawn do
         loop do
-          tuple = @chan.receive
-          process_event tuple[0], tuple[1]
+          event = @chan.receive
+          process_event event
         end
       end
     end
 
-    private def process_event(event : Event, callback)
-      puts "received #{@state} : #{event}"
-      state = @state
+    private def process_event(event : Event)
+      prev_state = @state
       case {state, event}
 
       when {State::STOPPED, Event::START}
         @state = State::STARTING
-        @start_proc.call callback
+        @start_proc.call
 
       when {State::FATAL, Event::START}
         @state = State::STARTING
-        @start_proc.call callback
+        @start_proc.call
 
       when {State::EXITED, Event::START}
         @state = State::STARTING
-        @start_proc.call callback
+        @start_proc.call
 
       when {State::STARTING, Event::STARTED}
         @state = State::RUNNING
@@ -87,26 +92,24 @@ module Supervisor
         @state = State::BACKOFF
         @try_count += 1
         if @try_count >= @retries
-          fire Event::FATAL, callback
+          fire Event::FATAL
         else
-          fire Event::RETRY, callback
+          fire Event::RETRY
         end
 
       when {State::STARTING, Event::STOP}
         @state = State::STOPPING
-        @stop_callback = callback
         @stop_proc.call
 
       when {State::BACKOFF, Event::RETRY}
         @state = State::STARTING
-        @start_proc.call callback
+        @start_proc.call
 
       when {State::BACKOFF, Event::FATAL}
         @state = State::FATAL
 
       when {State::RUNNING, Event::STOP}
         @state = State::STOPPING
-        @stop_callback = callback
         @stop_proc.call
 
       when {State::RUNNING, Event::EXITED}
@@ -115,18 +118,11 @@ module Supervisor
 
       when {State::STOPPING, Event::EXITED}
         @state = State::STOPPED
-        stop_callback = @stop_callback
-        if stop_callback
-          stop_callback.call(state, @state)
-        end
       else
         puts "unknown state/event combo: #{@state}, #{event}"
       end
 
-      if callback
-        callback.call(state, @state)
-      end
-
+      @state_stream.publish(prev_state, @state)
     end
   end
 end
