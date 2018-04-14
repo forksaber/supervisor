fbf5a73df3e7bd0208f9ec62fd549ae127478537
Neeraj <neeraj.bhunwal@gmail.com>
2018-04-14 16:33:19 +0530

    client: refactor call/rpc_call

--- src/supervisor/client.cr
+++ src/supervisor/client.cr
@@ -5,65 +5,31 @@ require "./net_string"
 module Supervisor
   class Client
 
-    alias RpcResponse = NamedTuple(status: String, data: JSON)
     alias CallArgs = Array(String | Bool | Int32)
-    alias ParseType = (JSON::Any | String)
 
     def initialize
       @server_path = "tmp/sv.sock"
     end
 
-    def call(name : String, args = CallArgs.new)
-      ok, data = rpc_call(name, args)
+    def call(name : String, args = CallArgs.new, response_type : T.class = JSON::Any) forall T
+      response = rpc_call(name, args)
+      ok, data = Tuple(Bool, T).from_json(response)
       if ! ok
         raise RpcException.new("Error in call #{name}(#{args}): #{data}")
       end
       data
-    end
-
-    def call2(name : String, args = CallArgs.new)
-      ok, data = rpc_call2(name, args)
-      if ! ok
-        raise RpcException.new("Error in call #{name}(#{args}): #{data}")
-      end
-      data
-    end
-
-    def call_safe(name : String, args = CallArgs.new, parse_type = ParseType)
-      rpc_call(name, args, parse_type)
-    end
-
-    private def rpc_call(name : String, args : CallArgs) : {Bool, (JSON::Any | String)}
-      json = {name: name, args: args}.to_json
-      connection << NetString.build(json)
-      response = NetString.read(connection)
-      if ! response
-        return {false, "EOFError"}
-      end
-      t = Tuple(Bool, ParseType).from_json(response)
-      return t
-    rescue e : MalformedNetString
-      message = e.message || "malformed net string"
-      {false, message}
     rescue e : JSON::ParseException
-      {false, "json parse error"}
+      raise RpcException.new(e.message)
     end
 
-    private def rpc_call2(name : String, args : CallArgs) : {Bool, SerializedRegistry}
+    private def rpc_call(name : String, args : CallArgs)
       json = {name: name, args: args}.to_json
       connection << NetString.build(json)
       response = NetString.read(connection)
-      if ! response
-        return {false, SerializedRegistry.new(current_group: "", old_groups: Array(String).new, state: SerializedState.new)}
-#        return {false, SerializedRegistry.new}
-      end
-      t = Tuple(Bool, SerializedRegistry).from_json(response)
-      return t
-#    rescue e : MalformedNetString
-#      message = e.message || "malformed net string"
-#      {false, message}
-#    rescue e : JSON::ParseException
-#      {false, "json parse error"}
+      raise RpcException.new("connection closed before sending response") if ! response
+      response
+    rescue e : MalformedNetString
+      raise RpcException.new(e.message)
     end
 
     private def connection
